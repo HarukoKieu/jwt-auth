77bc6d47ddfe0bc25632672aee2b2213e954a569
HarukoKieu <bethuong426@gmail.com>
2026-05-24 20:02:17 +0700

    update

--- backend/src/mail/mailtrap.config.js
+++ backend/src/mail/mailtrap.config.js
@@ -1,6 +1,5 @@
 import { MailtrapClient } from "mailtrap";
 import dotenv from "dotenv";
-import nodemailer from "nodemailer";
 
 dotenv.config();
 
@@ -11,17 +10,5 @@ export const mailtrapClient = new MailtrapClient({
 
 export const sender = {
   email: "mailtrap@demomailtrap.com",
-  name: "Demo Mailtrap",
+  name: "haruko",
 };
-
-export const transporter = nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: process.env.MAILTRAP_USER,
-    pass: process.env.MAILTRAP_PASS,
-  },
-});
-
-console.log("USER:", process.env.MAILTRAP_USER);
-console.log("PASS:", process.env.MAILTRAP_PASS);
